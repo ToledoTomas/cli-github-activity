16d41e27ef3cd09bb9100f7a57313a632763ba0f
unknown <tomas_toledo98@hotmail.com>
2024-09-29 15:12:03 -0300

    manejo de errores User

--- src/index.js
+++ src/index.js
@@ -2,26 +2,37 @@
 
 // const axios = require("axios");
 
-const { program } = require("commander");
-
 // const api = async user => {
 //   const event = await axios.get(`https://api.github.com/users/${user}/events`);
 //   const result = event.data;
 //   console.log(result);
 // };
 
-const api = user => {
-  fetch(`https://api.github.com/users/${user}/events`).then(response => {
-    response.json().then(data => {
-      console.log(data);
-    });
-  });
+const { program } = require("commander");
+
+const api = async user => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${user}/events`);
+    if (!response.ok) {
+      throw new Error("User not found");
+    }
+
+    const result = await response.json();
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-program.version("0.0.1").description("CLI Activity");
+try {
+  program.version("0.0.2").description("CLI Activity");
 
-program
-  .argument("<username>, Github username ")
-  .action(username => api(username));
+  program
+    .argument("<username>, Github username ")
+    .action(username => api(username));
 
-program.parse(process.argv);
+  program.parse(process.argv);
+} catch (error) {
+  console.error(`Ocurrio un error: ${error.message}`);
+}
